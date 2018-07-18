032635e8f8bdaee63ad7659a1c2d8c3fcb2c0940
asrbtjd <32615185+asrbtjd@users.noreply.github.com>
2018-07-18 09:23:15 +0900

    Add files via upload

--- 최규성/day11/UserVO.java
+++ 최규성/day11/UserVO.java
@@ -1,8 +1,53 @@
-package kr.co.mlec.day11;
+package day11;
 
-public class UserVO {
+import java.io.Serializable;
+
+public class UserVO implements Serializable {
 
 	private String name;
 	private String phone;
 	private String addr;
+	
+	public UserVO() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public UserVO(String name, String phone, String addr) {
+		super();
+		this.name = name;
+		this.phone = phone;
+		this.addr = addr;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public String getAddr() {
+		return addr;
+	}
+
+	public void setAddr(String addr) {
+		this.addr = addr;
+	}
+
+	@Override
+	public String toString() {
+		return "UserVO [name=" + name + ", phone=" + phone + ", addr=" + addr + "]";
+	}
+	
+	
 }
